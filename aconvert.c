1f03f16725a9cb6276533c6e9f73cd8f29f3caaa
Enno Rehling <enno.rehling@gmail.com>
2023-08-10 06:30:27 +0200

    initial commit

---
+++ aconvert.c
@@ -0,0 +1,9 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "rgba_bitmap/rgba_bitmap.h"
+
+int main(int argc, char **argv) {
+    return 0;
+}
